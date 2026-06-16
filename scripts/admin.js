b3555f1ce8da3040f02d04d519b90430c6ab236a
Vitor Meira Alves <alemaomeira197@gmail.com>
2026-06-15 21:33:19 -0300

    paginas da ana e textos ao inves de Lorem

--- scripts/admin.js
+++ scripts/admin.js
@@ -5,10 +5,8 @@ function fazerLogin(event) {
     const senha = document.getElementById('password').value;
 
     if (usuario === 'administrador' && senha === '1234') {
-        window.alert("Olá ADM")
         window.location.href = 'area_adm.html';
     }else if(usuario === 'associado' && senha === '1234') {
-        window.alert("Olá Associado")
         window.location.href = 'area_associado.html';
     } else {
         alert('Usuário ou senha incorretos!');
